1a2c31b2c22787afeb0f80332afe5b81604615fd
Adrien Lecharpentier <adrien.lecharpentier@gmail.com>
2014-04-07 17:56:17 +0200

    Fix missing block

--- app/scripts/vigenere-service.js
+++ app/scripts/vigenere-service.js
@@ -31,7 +31,9 @@ angular.module('ng.vigenere', [])
         if(c.charCodeAt(0) >= 'A'.charCodeAt(0) && c.charCodeAt(0) <= 'Z'.charCodeAt(0)) {
           var keyCode = key.charCodeAt((i - escaped) % key.length) - 'A'.charCodeAt(0);
           var code = ((value.charCodeAt(i) - 'A'.charCodeAt(0)) - keyCode) % 26;
-          if (code < 0) code += 26;
+          if (code < 0) {
+            code += 26;
+          }
           decrypted += String.fromCharCode('A'.charCodeAt(0) + code);
         } else {
           escaped += 1;
