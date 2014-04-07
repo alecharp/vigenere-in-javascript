cca2d61ed7349393b8c1b288c98ac02a012a5a45
Adrien Lecharpentier <adrien.lecharpentier@gmail.com>
2014-04-07 15:38:34 +0200

    Remove ngRoute as single view app

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,3 @@
 'use strict';
 
-angular.module('vigenereJavascriptApp', ['ngRoute'])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+angular.module('vigenereJavascriptApp', []);
