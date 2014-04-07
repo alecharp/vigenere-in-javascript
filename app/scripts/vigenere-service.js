680e26364cd3f4b453d335f5d0f9d6ec5b5fd366
Adrien Lecharpentier <adrien.lecharpentier@gmail.com>
2014-04-07 17:48:08 +0200

    Fix decryption method to correctly decrypt with Vigenere algorithm

--- app/scripts/vigenere-service.js
+++ app/scripts/vigenere-service.js
@@ -22,7 +22,23 @@ angular.module('ng.vigenere', [])
       return encrypted;
     };
     inst.decrypt = function(key, value) {
-      return key + '-' + value;
+      value = value.toUpperCase();
+      key = key.toUpperCase();
+      var decrypted = '';
+      var escaped = 0;
+      for (var i = 0; i<value.length; i++) {
+        var c = value.charAt(i);
+        if(c.charCodeAt(0) >= 'A'.charCodeAt(0) && c.charCodeAt(0) <= 'Z'.charCodeAt(0)) {
+          var keyCode = key.charCodeAt((i - escaped) % key.length) - 'A'.charCodeAt(0);
+          var code = ((value.charCodeAt(i) - 'A'.charCodeAt(0)) - keyCode) % 26;
+          if (code < 0) code += 26;
+          decrypted += String.fromCharCode('A'.charCodeAt(0) + code);
+        } else {
+          escaped += 1;
+          decrypted += c;
+        }
+      }
+      return decrypted.toLowerCase();
     };
     return inst;
   }]);
