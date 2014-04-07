7294af270daf7392a8ca9ebcfc48f527132b0f57
Adrien Lecharpentier <adrien.lecharpentier@gmail.com>
2014-04-07 17:59:14 +0200

    Add default value for clear text

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,6 +3,7 @@
 angular.module('vigenereJavascriptApp')
   .controller('MainCtrl', ['$scope', '$vigenere', function ($scope, $vigenere) {
     $scope.key = 'MUSIQUE';
+    $scope.clearText = 'j\'adore ecouter la radio toute la journee';
 
     $scope.encrypt = function(text) {
       $scope.encryptedText = $vigenere.encrypt($scope.key, text);
