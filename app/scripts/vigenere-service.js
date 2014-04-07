1299a19414617343741cfde42b490fee71ab3bcd
Adrien Lecharpentier <adrien.lecharpentier@gmail.com>
2014-04-07 16:59:06 +0200

    Fix encryption method to correctly encrypt with Vigenere algorithm

--- app/scripts/vigenere-service.js
+++ app/scripts/vigenere-service.js
@@ -4,7 +4,21 @@ angular.module('ng.vigenere', [])
   .factory('$vigenere', [function() {
     var inst = {};
     inst.encrypt = function(key, value) {
-      return key + '+' + value;
+      value = value.toUpperCase();
+      var encrypted = '';
+      var escaped = 0;
+      for (var i = 0; i<value.length; i++) {
+        var c = value.charAt(i);
+        if (c.charCodeAt(0) >= 'A'.charCodeAt(0) && c.charCodeAt(0) <= 'Z'.charCodeAt(0)) {
+          var keyCode = key.charCodeAt((i - escaped) % key.length) - 'A'.charCodeAt(0);
+          var code = ((value.charCodeAt(i) - 'A'.charCodeAt(0)) + keyCode) % 26;
+          encrypted += String.fromCharCode('A'.charCodeAt(0) + code);
+        } else {
+          escaped += 1;
+          encrypted += c;
+        }
+      }
+      return encrypted;
     };
     inst.decrypt = function(key, value) {
       return key + '-' + value;
