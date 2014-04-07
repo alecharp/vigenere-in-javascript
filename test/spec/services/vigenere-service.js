37e2b01d407310c685d0bc1ab29470f36de47ab4
Adrien Lecharpentier <adrien.lecharpentier@gmail.com>
2014-04-07 22:20:28 +0200

    Update tests to validate uppercase / lowercase
    The algorithm must be case sensitive

--- test/spec/services/vigenere-service.js
+++ test/spec/services/vigenere-service.js
@@ -17,4 +17,18 @@ describe('Vigenere Service', function() {
 		var crypted = 'V\'UVWHY IOIMBUL PM LSLYI XAOLM BU NAOJVUY';
 		expect(vigenere.decrypt('MUSIQUE', crypted)).toBe('j\'adore ecouter la radio toute la journee');
 	});
+
+	it('should keep case while encryption', function() {
+		var clearText = 'BonJour!? Je NE penSe pas TrouvER';
+		expect(vigenere.encrypt('MOTCLE', clearText)).toBe('NcgLzyd!? Xx PP tqbLg aee HkqfzQF');
+		expect(vigenere.encrypt('motcle', clearText)).toBe('NcgLzyd!? Xx PP tqbLg aee HkqfzQF');
+		expect(vigenere.encrypt('MoTCLe', clearText)).toBe('NcgLzyd!? Xx PP tqbLg aee HkqfzQF');
+	});
+
+	it('should keep case while decryption', function() {
+		var clearText = 'NcgLzyd!? Xx PP tqbLg aee HkqfzQF';
+		expect(vigenere.encrypt('MOTCLE', clearText)).toBe('BonJour!? Je NE penSe pas TrouvER');
+		expect(vigenere.encrypt('motcle', clearText)).toBe('BonJour!? Je NE penSe pas TrouvER');
+		expect(vigenere.encrypt('MoTCLe', clearText)).toBe('BonJour!? Je NE penSe pas TrouvER');
+	});
 });
