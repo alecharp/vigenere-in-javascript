817e2ef9f8d922d5b2216d71bac20405f1e3c04b
Adrien Lecharpentier <adrien.lecharpentier@gmail.com>
2014-04-08 20:48:40 +0200

    Re-integrate routes to have multiple views

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,8 @@
 'use strict';
 
-angular.module('vigenereJavascriptApp', ['ng.vigenere']);
+angular.module('vigenereJavascriptApp', ['ng.vigenere', 'ngRoute'])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/index',  {templateUrl: 'views/main.html'})
+      .otherwise({redirectTo: '/index'});
+  }]);
